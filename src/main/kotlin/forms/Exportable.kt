5ea261299cf31497be41fddde8bd81a80b5c7545
Wyatt Phillips <wphillips@128.dev>
2023-01-21 20:12:44 -0500

    Simplify Form and remove Exportable

--- src/main/kotlin/forms/Exportable.kt
+++
@@ -1,8 +0,0 @@
-package forms
-
-import java.util.stream.Stream
-
-interface Exportable {
-    fun export(): Stream<String>
-    fun import(stream: Stream<String>)
-}
--- src/main/kotlin/forms/Form.kt
+++ src/main/kotlin/forms/Form.kt
@@ -1,15 +1,5 @@
 package forms
 
-import java.util.stream.Stream
-
-open class Form(var taxYear: Int) : Exportable {
+open class Form(var taxYear: Int) {
     var locked: Boolean = false
-
-    override fun export(): Stream<String> {
-        TODO("Not yet implemented")
-    }
-
-    override fun import(stream: Stream<String>) {
-        TODO("Not yet implemented")
-    }
 }
